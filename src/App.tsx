97b2c265dca5446caedb3a37c1d3adeb263b324b
Nick <nsagias@gmail.com>
2023-03-18 17:40:06 -0400

    create containers and spacing

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import './App.css';
 import Alert from './components/Alert/Alert';
 import AlertTailwind from './components/Alert/AlertTailwind';
 import Colors from './components/Colors/Colors';
+import Containers from './components/Containers/Containers';
 
 function App() {
   return (
     <div className="App">
-      <Colors />
+      <Containers />
     </div>
   );
 }
---
+++ src/components/Containers/Containers.tsx
@@ -0,0 +1,15 @@
+export default function Containers() {
+  return (
+    <div>
+      <article>
+        <h3>Article</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem laboriosam libero molestiae recusandae accusantium
+          voluptates? Expedita dignissimos amet eveniet dolore nobis odio a
+          sunt, maiores quasi. Modi amet quos dolores!
+        </p>
+      </article>
+    </div>
+  );
+}
