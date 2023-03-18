b54df46d4641d0d867e113ce3673ba076f313188
Nick <nsagias@gmail.com>
2023-03-18 16:18:52 -0400

    add borders and divide

--- src/components/Colors/Colors.tsx
+++ src/components/Colors/Colors.tsx
@@ -18,11 +18,27 @@ export default function Colors() {
       <p className="bg-yellow-300">{boilerPlate}</p>
       <p className="bg-indigo-800 text-white">{boilerPlate}</p>
 
-      {/* Text underline */}
+      {/* Text Underline */}
       <h1 className="mt-12"></h1>
       <p className="underline decoration-red-500">{boilerPlate}</p>
       <p className="underline decoration-emerald-600">{boilerPlate}</p>
       <p className="underline decoration-yellow-800">{boilerPlate}</p>
+
+      {/* Color Borders */}
+      <h1 className="mt-12"></h1>
+      <input type="text" className="border border-blue-400" />
+      <input type="text" className="border border-green-400" />
+      <input type="text" className="border border-orange-400" />
+
+      {/* Divide Colors */}
+      <h1 className="mt-12"></h1>
+      <div className="divide-y divide-blue-300">
+        <div>Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+        <div>Item 4</div>
+        <div>Item 5</div>
+      </div>
     </div>
   );
 }
