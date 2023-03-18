47cad2b2b84dc7612a41f8f43e2f708c1604774b
Nick <nsagias@gmail.com>
2023-03-18 14:24:29 -0400

    create alert button in css

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,5 +1,18 @@
+import { FiAlertTriangle } from "react-icons/fi";
+
+import "./Alert.css"
 export default function Alert() {
   return (
-    <h1>Alert</h1>
+    <>
+    <div className="alert alert-postion">
+      <div className="alert-log-wrap">
+        <FiAlertTriangle className="alert-logo" />
+      </div>
+      <div className="alert-body">
+        <h4 className="alert-title">Confirm</h4>
+        <p className="alert-message">Are you sure you want to proceed?</p>
+      </div>
+    </div>
+    </>
   );
 }
