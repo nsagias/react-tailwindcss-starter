120363d7dae408440d63742e515f12d031a63908
Nick <nsagias@gmail.com>
2023-03-18 13:57:07 -0400

    create alert component and alert.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Alert from './components/Alert/Alert';
 
 function App() {
   return (
     <div className="App">
-     <h1>here</h1>
+      <Alert />
     </div>
   );
 }
