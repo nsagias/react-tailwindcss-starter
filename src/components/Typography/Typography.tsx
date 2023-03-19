dddc3cab32b4a930349a6963f0937b7a5e7cde71
Nick <nsagias@gmail.com>
2023-03-19 18:12:18 -0400

    style section names

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -2,19 +2,19 @@ export default function Typography() {
   const boilerplate = "Tailwind typography";
   return (
     <div>
-      <h1>Typography</h1>
+      <h1 className="flex justify-center text-xl font-bold">Typography</h1>
       <hr />
 
       {/* Font Family */}
     
-      <div className="mt-2">Font Family</div>
+      <div className="text-lg font-bold underline mt-2">Font Family</div>
       <hr />
       <div className="font-sans">font-sans: {boilerplate}</div>
       <div className="font-serif">font-serif: {boilerplate}</div>
       <div className="font-mono">font-mono: {boilerplate}</div>
 
       {/* Font Size */}
-      <div className="mt-2">Font Size</div>
+      <div className="text-lg font-bold underline mt-2">Font Size</div>
       <hr />
       <div className="text-xs">text-xs: {boilerplate}</div>
       <div className="text-sm">text-sm: {boilerplate}</div>
@@ -26,31 +26,36 @@ export default function Typography() {
 
 
       {/* Font Weight */}
-      <div className="mt-2">Font Weight</div>
+      <div className="text-lg font-bold underline mt-2">Font Weight</div>
       <hr />
+      <div className="font-light">font-light: {boilerplate}</div>
+      <div className="font-normal">font-normal: {boilerplate}</div>
+      <div className="font-medium">font-medium: {boilerplate}</div>
+      <div className="font-semibold">font-semibold: {boilerplate}</div>
+      <div className="font-bold">font-bold: {boilerplate}</div>
 
       {/* Letter Spacing */}
-      <div className="mt-2">Letter Spacing</div>
+      <div className="text-lg font-bold underline mt-2">Letter Spacing</div>
       <hr />
 
       {/* Text Alignment */}
-      <div className="mt-2">Text Alignment</div>
+      <div className="text-lg font-bold underline mt-2">Text Alignment</div>
       <hr />
 
       {/* Text Decoration */}
-      <div className="mt-2">Text Decoration</div>
+      <div className="text-lg font-bold underline mt-2">Text Decoration</div>
       <hr />
 
       {/* Decoration Style */}
-      <div className="mt-2">Decoration Style</div>
+      <div className="text-lg font-bold underline mt-2">Decoration Style</div>
       <hr />
 
       {/* Decoration Offset */}
-      <div className="mt-2">Decoration Offset</div>
+      <div className="text-lg font-bold underline mt-2">Decoration Offset</div>
       <hr />
 
       {/* Text Transform */}
-      <div className="mt-2">Text Transform</div>
+      <div className="text-lg font-bold underline mt-2">Text Transform</div>
       <hr />
    </div>
   );
