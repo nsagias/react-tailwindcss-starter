86bdec48a5ab9bb71441ee30de7e7af8061c3e74
Nick <nsagias@gmail.com>
2023-03-19 20:45:00 -0400

    add LayoutPosition component, scaffold and add relative and absolute example

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import Alert from './components/Alert/Alert';
 import AlertTailwind from './components/Alert/AlertTailwind';
 import Colors from './components/Colors/Colors';
 import Containers from './components/Containers/Containers';
+import LayoutPosition from './components/LayoutPosition/LayoutPosition';
 import Sizing from './components/Sizing/Sizing';
 import Typography from './components/Typography/Typography';
 
 export default function App() {
   return (
     <div className="App">
-      <Sizing />
+    <LayoutPosition />
     </div>
   );
 }
---
+++ src/components/LayoutPosition/LayoutPosition.tsx
@@ -0,0 +1,50 @@
+export default function LayoutPosition() {
+  const b = "Tailwind";
+  return (
+    <div>
+      <h1 className="flex justify-center text-xl font-bold">Layout Position</h1>
+      <hr />
+
+      {/* Positioning */}
+      <p className="text-lg font-bold underline mt-2">Positioning</p>
+      <div className="relative h-12 w-1/2 bg-red-400 text-white font-bold">
+        <p>Relative Parent Element</p>
+        <div className="absolute bottom-0 right-0 bg-red-900 text-white font-bold">
+          <p>Absolute Child Element</p>
+        </div>
+      </div>
+
+      {/* Top Left Corner */}
+      <p className="text-lg font-bold underline mt-2">Top Left Corner</p>
+      <div className="relative h-32 w-32 bg-yellow-100">
+        <div className="absolute left-0 top-0 h-16 w-16 bg-yellow-300"></div>
+      </div>
+
+      {/* Top Right Corner */}
+      <p className="text-lg font-bold underline mt-2">Top Right Corner</p>
+      <div className="relative h-32 w-32 bg-yellow-100">
+        <div className="absolute right-0 top-0 h-16 w-16 bg-yellow-300"></div>
+      </div>
+      {/* Bottom Left Corner */}
+      <p className="text-lg font-bold underline mt-2">Bottom Left Corner</p>
+      <div className="relative h-32 w-32 bg-yellow-100">
+        <div className="absolute left-0 bottom-0 h-16 w-16 bg-yellow-300"></div>
+      </div>
+      {/* Bottom Right Corner */}
+      <p className="text-lg font-bold underline mt-2">Bottom Right Corner</p>
+      <div className="relative h-32 w-32 bg-yellow-100">
+        <div className="absolute right-0 bottom-0 h-16 w-16 bg-yellow-300"></div>
+      </div>
+      {/* Span Top Edge */}
+      <p className="text-lg font-bold underline mt-2">Span Top Edge</p>
+      {/* Span Left Edge */}
+      <p className="text-lg font-bold underline mt-2">Span Left Edge</p>
+      {/* Span Right Edge */}
+      <p className="text-lg font-bold underline mt-2">Span Right Edge</p>
+      {/* Span Bottom Edge */}
+      <p className="text-lg font-bold underline mt-2">Span Bottom Edge</p>
+
+
+    </div>
+  );
+}
