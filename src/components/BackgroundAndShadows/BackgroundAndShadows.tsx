6afceef38ed6a1bd425043760c38e20fdc167ebb
Nick <nsagias@gmail.com>
2023-03-20 01:50:03 -0400

    add backgroun image to div

--- src/components/BackgroundAndShadows/BackgroundAndShadows.tsx
+++ src/components/BackgroundAndShadows/BackgroundAndShadows.tsx
@@ -1,7 +1,12 @@
 export default function BackgroundAndShadows() {
   return (
-   <div className="font-bold text-xl underline">
-      Background and Shadows
-   </div>
+    <>
+      <div 
+        className="h-64 w=[1000px] bg-cover bg-no-repeat bg-center"
+        style={{backgroundImage: `url(${"https://images.unsplash.com/photo-1526726538690-5cbf956ae2fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNhcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60"})`}}>
+          <span className="font-bold text-3xl bg-black text-white p-2">Background And Shadows</span> 
+      </div>
+
+    </>
   );
 }
