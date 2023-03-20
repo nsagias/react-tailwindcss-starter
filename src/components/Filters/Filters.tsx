7c1c0a63e727ffac9f2baa3b92000970b596081d
Nick <nsagias@gmail.com>
2023-03-20 17:59:41 -0400

    update section titles text-2xl mt-8

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -3,10 +3,10 @@ export default function Filters() {
   const carImageUnsplash1 = "https://images.unsplash.com/photo-1526726538690-5cbf956ae2fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNhcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60";
   return (
     <>
-      <p className="font-bold text-xl underline">Filters</p>
+      <p className="font-bold text-3xl underline">Filters</p>
 
       {/* Blur */}
-      <p className="font-bold text-lg">Blur</p>
+      <p className="font-bold text-2xl mt-8">Blur</p>
       <div className="blur-none">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi debitis, dignissimos molestias accusantium praesentium repellat ipsam magni iure dolorum voluptates!</div>
       
       <div className="blur-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, illo dolorem beatae numquam alias accusantium nobis voluptate labore harum non.</div>
@@ -18,7 +18,7 @@ export default function Filters() {
       <img src={carImageUnsplash1} alt="cars" className="blur-sm" />
 
       {/* Brightness */}
-      <p className="font-bold text-lg">Brightness</p>
+      <p className="font-bold text-2xl mt-8">Brightness</p>
       <img src={carImageUnsplash1} alt="" className="brightness-100" />
       <img src={carImageUnsplash1} alt="" className="brightness-50" />
       <img src={carImageUnsplash1} alt="" className="brightness-75" />
@@ -26,19 +26,20 @@ export default function Filters() {
       <img src={carImageUnsplash1} alt="" className="brightness-200" />
 
       {/* Contrast */}
-      <p className="font-bold text-lg">Contrast</p>
+      <p className="font-bold text-2xl mt-8">Contrast</p>
+
 
       {/* Grayscale */}
-      <p className="font-bold text-lg">Grayscale</p>
+      <p className="font-bold text-2xl mt-8">Grayscale</p>
 
       {/* Invert */}
-      <p className="font-bold text-lg">Ivert</p>
+      <p className="font-bold text-2xl mt-8">Ivert</p>
 
       {/* Sepia */}
-      <p className="font-bold text-lg">Sepia</p>
+      <p className="font-bold text-2xl mt-8">Sepia</p>
 
       {/* Hue Rotate */} 
-      <p className="font-bold text-lg">Hue Rotate</p>
+      <p className="font-bold text-2xl mt-8">Hue Rotate</p>
     </>
   );
 }
