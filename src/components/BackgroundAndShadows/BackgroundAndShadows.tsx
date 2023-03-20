201aea6f21bde62621f8560c96210f4b6c98ac7a
Nick <nsagias@gmail.com>
2023-03-20 01:50:27 -0400

    add gradiant to div

--- src/components/BackgroundAndShadows/BackgroundAndShadows.tsx
+++ src/components/BackgroundAndShadows/BackgroundAndShadows.tsx
@@ -6,7 +6,7 @@ export default function BackgroundAndShadows() {
         style={{backgroundImage: `url(${"https://images.unsplash.com/photo-1526726538690-5cbf956ae2fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGNhcnN8ZW58MHx8MHx8&auto=format&fit=crop&w=700&q=60"})`}}>
           <span className="font-bold text-3xl bg-black text-white p-2">Background And Shadows</span> 
       </div>
-
+      <div className="h-24 bg-gradient-to-r from-cyan-500 to-blue-500 mt-2"></div>
     </>
   );
 }
