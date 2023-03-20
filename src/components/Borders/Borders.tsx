7bd3c988a9e0de62dad01878de23a2de2f9f2e85
Nick <nsagias@gmail.com>
2023-03-20 02:37:24 -0400

    fix typo

--- src/components/Borders/Borders.tsx
+++ src/components/Borders/Borders.tsx
@@ -15,30 +15,30 @@ export default function Borders() {
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, doloribus?
     </div>
 
-    <div className="w-95 m-3 p-5 border-x-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, incidunt.</div>
+    <div className="w-96 m-3 p-5 border-x-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, incidunt.</div>
 
-    <div className="w-95 m-3 p-5 border-y-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, veniam.</div>
+    <div className="w-96 m-3 p-5 border-y-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, veniam.</div>
 
-    <div className="w-94 m-3 p-5 border-t-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, temporibus.</div>
+    <div className="w-96 m-3 p-5 border-t-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, temporibus.</div>
 
-    <div className="w-94 m-3 p-5 border-b-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, quasi!</div>
+    <div className="w-96 m-3 p-5 border-b-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, quasi!</div>
 
-    <div className="w-94 m-3 p-5 border-r-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, molestias.</div>
+    <div className="w-96 m-3 p-5 border-r-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, molestias.</div>
 
-    <div className="w94 m-3 p-5 border-l-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ducimus?</div>
+    <div className="w-96 m-3 p-5 border-l-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ducimus?</div>
 
     {/* Border Radius */}
-    <div className="w-94 m-3 p-5 border-4 rounded">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, temporibus?</div>
+    <div className="w-96 m-3 p-5 border-4 rounded">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, temporibus?</div>
 
-    <div className="w-94 m-3 p-5 border-4 rounded-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, unde.</div>
+    <div className="w-96 m-3 p-5 border-4 rounded-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, unde.</div>
 
-    <div className="w-94 m-3 p-5 border-4 rounded-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, deleniti?</div>
+    <div className="w-96 m-3 p-5 border-4 rounded-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, deleniti?</div>
 
-    <div className="w-94 m-3 p-5 border-4 rounded-full">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, quas.</div>
+    <div className="w-96 m-3 p-5 border-4 rounded-full">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, quas.</div>
 
-    <div className="w-94 m-3 p-5 border-4 rounded-t-2xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, itaque!</div>
+    <div className="w-96 m-3 p-5 border-4 rounded-t-2xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, itaque!</div>
 
-    <div className="w94 m-3 p-5 border-4 rounded-b-2xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus, magni!</div>
+    <div className="w-96 m-3 p-5 border-4 rounded-b-2xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus, magni!</div>
 
     {/* Outline */}
     <button className="m-3 p-5 outline outline-4 outline-red-500 rounded-full">Click Me!</button>
