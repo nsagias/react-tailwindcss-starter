8f1d2ede339fe3ff717d11a7bf785d0d01233104
Nick <nsagias@gmail.com>
2023-03-20 02:30:42 -0400

    add border rounded types

--- src/components/Borders/Borders.tsx
+++ src/components/Borders/Borders.tsx
@@ -28,6 +28,17 @@ export default function Borders() {
     <div className="w94 m-3 p-5 border-l-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ducimus?</div>
 
     {/* Border Radius */}
+    <div className="w-94 m-3 p-5 border-4 rounded">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, temporibus?</div>
+
+    <div className="w-94 m-3 p-5 border-4 rounded-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, unde.</div>
+
+    <div className="w-94 m-3 p-5 border-4 rounded-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, deleniti?</div>
+
+    <div className="w-94 m-3 p-5 border-4 rounded-full">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, quas.</div>
+
+    <div className="w-94 m-3 p-5 border-4 rounded-t-2xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, itaque!</div>
+
+    <div className="w94 m-3 p-5 border-4 rounded-b-2xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus, magni!</div>
     {/* Outline */}
     </>
   );
