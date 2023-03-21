1bfe2508df71b57840e8f1a201fa8fcb7348691c
Nick <nsagias@gmail.com>
2023-03-21 18:43:44 -0400

    add flex-initial, flex-none, flex-autp, flex-1

--- src/components/flex/FlexShrink.tsx
+++ src/components/flex/FlexShrink.tsx
@@ -5,12 +5,16 @@ export default function FlexShrink () {
 
       <p className="text-lg font-bold underlinem mt-2">Flex Shrink</p>
       
-      <div className="flex flex-col gap-4 items-center justify-between h-72 w-full bg-gray-100 mt-2">
-        <div className="order-3 p-10 border border-blue-400 bg-blue-100">01</div>
-        <div className="order-2 p-10 border border-blue-400 bg-blue-100">02</div>
-        <div className="order-4 p-10 border border-blue-400 bg-blue-100">03</div>
-        <div className="order-1 p-10 border border-blue-400 bg-blue-100">04</div>
+      <div className="flex w-full bg-gray-100 mt-2">
+        <div className="flex-initial w-64 p-10 border border-blue-400 bg-blue-100">01</div>
+        <div className="flex-none w-64 p-10 border border-blue-400 bg-blue-100">02</div>
+        <div className="flex-auto w-64 p-10 border border-blue-400 bg-blue-100">03</div>
+        <div className="flex-1 w-64 p-10 border border-blue-400 bg-blue-100">04</div>
+        <div className="p-10 border border-blue-400 bg-blue-100">05</div>
+        <div className="p-10 border border-blue-400 bg-blue-100">06</div>
+        <div className="p-10 border border-blue-400 bg-blue-100">07</div>
       </div>
+
     </div>
   );
 }
