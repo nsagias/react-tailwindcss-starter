c382c466a3204958a2bcf029404e634b47a89133
Nick <nsagias@gmail.com>
2023-03-20 23:51:14 -0400

    add break points for background and font size

--- src/components/Breakpoints/BreakPoints.tsx
+++ src/components/Breakpoints/BreakPoints.tsx
@@ -1,12 +1,13 @@
-import { useEffect, useState } from "react";
 import useWindowDimensions from "../../Utils/useWindowDimensions";
-
 
 export default function BreakPoints() {
 
+  const { width } = useWindowDimensions();
 
-  const { width } = useWindowDimensions()
   return (
-    <>{width}</>
+      <div className="bg-black sm:bg-green-800 md:bg-blue-800 lg:bg-red-900 xl:bg-purple-800 2xl:bg-yellow-500 w-screen h-screen">
+        <div className="h1 text-white text-xl md:texl-2xl lg:text-3xl font-bold">{width}</div>
+      </div>
+    
   );
 }
