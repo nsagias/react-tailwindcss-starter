d73d52903e4a30162cf1be84445f78fbbd6b3e2d
Nick <nsagias@gmail.com>
2023-03-20 20:09:01 -0400

    create breakpoints component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Alert from './components/Alert/Alert';
 import AlertTailwind from './components/Alert/AlertTailwind';
 import BackgroundAndShadows from './components/BackgroundAndShadows/BackgroundAndShadows';
 import Borders from './components/Borders/Borders';
+import BreakPoints from './components/Breakpoints/BreakPoints';
 import Colors from './components/Colors/Colors';
 import Containers from './components/Containers/Containers';
 import Fitlers from './components/Filters/Filters';
@@ -14,7 +15,7 @@ import Typography from './components/Typography/Typography';
 export default function App() {
   return (
     <div className="App">
-     <Interactivity />
+      <BreakPoints />
     </div>
   );
 }
