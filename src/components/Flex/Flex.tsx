940182c7ffff14b4f65bd9bda7dff8dc58d223fe
Nick <nsagias@gmail.com>
2023-03-21 17:51:11 -0400

    add flex, flex-col wth md:flex-row, and flex w-full

--- src/components/Flex/Flex.tsx
+++ src/components/Flex/Flex.tsx
@@ -2,6 +2,28 @@ export default function Flex () {
   return (
     <div >
       <p className="text-xl font-bold underline">Flex Box</p>
+
+      <div className="flex mt-2">
+        <div className="p-10 border border-blue-400 bg-blue-100">01</div>
+        <div className="p-10 border border-blue-400 bg-blue-100">02</div>
+        <div className="p-10 border border-blue-400 bg-blue-100">03</div>
+        <div className="p-10 border border-blue-400 bg-blue-100">04</div>
+      </div>
+
+      <div className="flex flex-col md:flex-row mt-2">
+        <div className="p-10 border border-blue-400 bg-blue-100">01</div>
+        <div className="p-10 border border-blue-400 bg-blue-100">02</div>
+        <div className="p-10 border border-blue-400 bg-blue-100">03</div>
+        <div className="p-10 border border-blue-400 bg-blue-100">04</div>
+      </div>
+
+      <div className="flex h-72 w-full bg-gray-100 mt-2">
+        <div className="p-10 border border-blue-400 bg-blue-100">01</div>
+        <div className="p-10 border border-blue-400 bg-blue-100">02</div>
+        <div className="p-10 border border-blue-400 bg-blue-100">03</div>
+        <div className="p-10 border border-blue-400 bg-blue-100">04</div>
+      </div>
+
     </div>
   );
 }
