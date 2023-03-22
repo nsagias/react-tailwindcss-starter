e202ed29821691e7291d9e44a681ff1a8e5597e2
Nick <nsagias@gmail.com>
2023-03-21 21:36:13 -0400

    add custom animation animate-wiggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,14 @@ module.exports = {
         2000: "2000ms",
       },
       animation: {
-        "spin-slow": "spin 3s linear infinite"
+        "spin-slow": "spin 3s linear infinite",
+        wiggle: "wiggle 1s ease-in-out infinite"
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-45deg)"},
+          "50%": { transform: "rotate(45deg)"}
+        },
       },
     },
     fontFamily: {
