3ec189240fda4ea6dbc7c615724bd471469b3a5c
Nick <nsagias@gmail.com>
2023-03-22 16:45:27 -0400

    create dark mode component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import './App.css';
 import Alert from './components/alert/Alert';
 import AlertTailwind from './components/alert/AlertTailwind';
 import Animations from './components/animations/Animations';
-
 import BackgroundAndShadows from './components/backgroundAndShadows/BackgroundAndShadows';
 import Borders from './components/borders/Borders';
 import BreakPoints from './components/breakpoints/BreakPoints';
 import Colors from './components/colors/Colors';
 import Columns from './components/columns/Columns';
 import Containers from './components/containers/Containers';
+import DarkMode from './components/darkMode/DarkMode';
 import Fitlers from './components/filters/Filters';
 import FlexAndAlignItems from './components/flex/FlexAndAlignItems';
 import FlexColumnGapAndOrder from './components/flex/FlexColumGapAndOrder';
@@ -24,7 +24,7 @@ import Typography from './components/typography/Typography';
 export default function App() {
   return (
     <div className="App">
-      <Animations />
+      <DarkMode />
     </div>
   );
 }
