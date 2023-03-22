91b49346dacd4c338c5dd8934f3f886db50e36db
Nick <nsagias@gmail.com>
2023-03-21 20:22:14 -0400

    create base for grid example

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -3,9 +3,16 @@ export default function Grid () {
     <div >
       <p className="text-xl font-bold underline">Grid</p>
 
-      
-      <div className="flex w-full bg-gray-100 mt-2">
-
+      <div className="grid bg-gray-100 mt-2">
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 1</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 2</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 3</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 4</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 5</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 6</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 7</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 8</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 9</div>
       </div>
 
     </div>
