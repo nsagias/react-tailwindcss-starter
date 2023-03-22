7a93749f3a76d705ceb5144c585279c325f593e9
Nick <nsagias@gmail.com>
2023-03-21 20:24:57 -0400

    add grid-col 2,3,4

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -3,7 +3,43 @@ export default function Grid () {
     <div >
       <p className="text-xl font-bold underline">Grid</p>
 
-      <div className="grid bg-gray-100 mt-2">
+      <div className="grid gap-2 bg-gray-100 mt-2">
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 1</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 2</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 3</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 4</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 5</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 6</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 7</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 8</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 9</div>
+      </div>
+
+      <div className="grid grid-cols-3 gap-2 bg-gray-100 mt-2">
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 1</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 2</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 3</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 4</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 5</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 6</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 7</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 8</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 9</div>
+      </div>
+
+      <div className="grid grid-cols-2 gap-2 bg-gray-100 mt-2">
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 1</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 2</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 3</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 4</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 5</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 6</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 7</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 8</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 9</div>
+      </div>
+
+      <div className="grid grid-cols-4 gap-2 bg-gray-100 mt-2">
         <div className="p-10 border border-blue-900 bg-blue-100">Item 1</div>
         <div className="p-10 border border-blue-900 bg-blue-100">Item 2</div>
         <div className="p-10 border border-blue-900 bg-blue-100">Item 3</div>
