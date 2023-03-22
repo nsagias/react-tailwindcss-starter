7cf602bf28524f5fb40d8e4b83507f20d13da334
Nick <nsagias@gmail.com>
2023-03-21 20:31:30 -0400

    add spcol spans

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -51,6 +51,31 @@ export default function Grid () {
         <div className="p-10 border border-blue-900 bg-blue-100">Item 9</div>
       </div>
 
+      <div className="grid grid-cols-3 grid-rows-6 gap-2 bg-gray-300 mt-2">
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 1</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 2</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 3</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 4</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 5</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 6</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 7</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 8</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 9</div>
+      </div>
+
+      <div className="grid grid-cols-3 bg-gray-300 mt-2">
+        <div className="col-span-2 row-span-2 p-10 border border-blue-900 bg-blue-100">Item 1</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 2</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 3</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 4</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 5</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 6</div>
+        <div className="col-span-2 p-10 border border-blue-900 bg-blue-100">Item 7</div>
+        <div className="p-10 border border-blue-900 bg-blue-100">Item 8</div>
+        <div className="col-span-3 p-10 border border-blue-900 bg-blue-100">Item 9</div>
+      </div>
+
+
     </div>
   );
 }
