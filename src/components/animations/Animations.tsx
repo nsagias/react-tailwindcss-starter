5ba6dc8dd69fc198f439576a41e8d615c3ef22ea
Nick <nsagias@gmail.com>
2023-03-21 21:13:34 -0400

    import image/font

--- src/components/animations/Animations.tsx
+++ src/components/animations/Animations.tsx
@@ -1,7 +1,12 @@
+import { FiAlertTriangle } from "react-icons/fi";
+
 export default function Animations() {
   return (
     <div>
       <p className="text-xl font-bold underline">Animation and Keyframes</p>
+      <div className="flex items-center justify-center min-h-screen bg-slate-900">
+        <FiAlertTriangle className="h-24 w-24 text-white" />
+      </div>
     </div>
   );
 }
