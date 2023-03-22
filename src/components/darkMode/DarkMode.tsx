4ac1ea72b7165734e07465e156958c155cc6663b
Nick <nsagias@gmail.com>
2023-03-22 16:55:25 -0400

    create darkmode component

--- src/components/darkMode/DarkMode.tsx
+++ src/components/darkMode/DarkMode.tsx
@@ -1,5 +1,10 @@
 export default function DarkMode() {
   return (
-    <div>Dark Mode Component</div>
+    <div>
+      <div className="container mx-auto mt-10 bg-white dark:bg-slate-900 rounded-xl px-6 py-8 shadow-xl">
+        <h3 className="flex justify-center text-lg font-bold text-slate-900 dark:text-white">Here</h3>
+        <p className="text-slate-500 dark:text-slate-400 font-medium tracking-tight">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas quidem quos aspernatur nulla? Voluptate magni quo perferendis sint esse incidunt.</p>
+      </div>
+    </div>
   );
 }
