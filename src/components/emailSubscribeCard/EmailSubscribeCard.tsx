8d3f01fe6b4bd74193691db33f13864d81578ced
Nick <nsagias@gmail.com>
2023-03-23 15:43:41 -0400

    create card and import left image

--- src/components/emailSubscribeCard/EmailSubscribeCard.tsx
+++ src/components/emailSubscribeCard/EmailSubscribeCard.tsx
@@ -1,5 +1,21 @@
 export default function EmailSubscribeCard() {
   return (
-    <div>Email Subscribe Card Component</div>
+    <div className="flex items-center justify-center h-screen bg-zinc-700">
+
+      {/* Email Card */}
+      <div className="bg-zinc-800 p-2 mx-6 rounded-2xl">
+
+        {/* Flex Container */}
+        <div className="flex flex-col md:flex-row rounded-l-xl">
+
+          {/* Left: Image */}
+          <img 
+            src="images/emailSubscribe/image.jpg" 
+            alt="" 
+            className="" 
+          />
+        </div>
+      </div>
+    </div>
   );
 }
