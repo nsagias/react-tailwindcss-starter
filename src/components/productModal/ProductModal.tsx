e1bb565d73c27e04addee0bd8d4043d786ed24f0
Nick <nsagias@gmail.com>
2023-03-23 19:41:53 -0400

    add image to component

--- src/components/productModal/ProductModal.tsx
+++ src/components/productModal/ProductModal.tsx
@@ -1,7 +1,21 @@
 export default function ProductModal() {
   return (
     <>
-    <div></div>
+      {/* Modal Container */}
+      <div className="flex items-center justify-center min-h-screen bg-slate-300">
+        
+        {/* Card Container: Product Modal */}
+        <div className="flex flex-col bg-white p-6 m-3 space-y-10 rounded-2xl shadow-2xl md:flex-row md:space-x-10 md:p-16 md:m-0">
+          
+          {/* Image: if not in div, streches */}
+          <div>
+            <img 
+              src="images/productModalImages/headphone.png" 
+              alt="headphones" 
+              className="mx-auto hover:scale-105 w-60 duraction-200" />
+          </div>  
+        </div>
+      </div>
     </>
   );
 }
