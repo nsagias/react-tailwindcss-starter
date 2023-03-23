06bf02e09338c1bd09353f88e61b5fdcecb68421
Nick <nsagias@gmail.com>
2023-03-23 15:20:25 -0400

    email suscribe card

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
 import './App.css';
-import Alert from './components/alert/Alert';
-import AlertTailwind from './components/alert/AlertTailwind';
-import Animations from './components/fundamentals/animations/Animations';
-import BackgroundAndShadows from './components/fundamentals/backgroundAndShadows/BackgroundAndShadows';
-import Borders from './components/fundamentals/borders/Borders';
-import BreakPoints from './components/fundamentals/breakpoints/BreakPoints';
-import Colors from './components/fundamentals/colors/Colors';
-import Columns from './components/fundamentals/columns/Columns';
-import Containers from './components/fundamentals/containers/Containers';
-import DarkMode from './components/darkMode/DarkMode';
-import Fitlers from './components/fundamentals/filters/Filters';
-import FlexAndAlignItems from './components/fundamentals/flex/FlexAndAlignItems';
-import FlexColumnGapAndOrder from './components/fundamentals/flex/FlexColumGapAndOrder';
-import FlexShrink from './components/fundamentals/flex/FlexShrink';
-import Grid from './components/fundamentals/grid/Grid';
-import GridSpans from './components/fundamentals/grid/GridSpans';
-import Interactivity from './components/fundamentals/interactivity/Interactivity';
-import LayoutPosition from './components/fundamentals/layoutPosition/LayoutPosition';
-import Sizing from './components/fundamentals/sizing/Sizing';
-import TransitionAndTransform from './components/fundamentals/transitionAndTransform/TransitionAndTransform';
-import Typography from './components/fundamentals/typography/Typography';
+import EmailSubscribeCard from './components/emailSubscribeCard/EmailSubscribeCard';
+
 
 export default function App() {
   return (
     <div className="App">
-      <DarkMode />
+      <EmailSubscribeCard />
     </div>
   );
 }
---
+++ src/components/emailSubscribeCard/EmailSubscribeCard.tsx
@@ -0,0 +1,5 @@
+export default function EmailSubscribeCard() {
+  return (
+    <div>Email Subscribe Card Component</div>
+  );
+}
