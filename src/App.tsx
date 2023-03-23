f8bc7d051db38ff1c29a2ef3c2c7e05b2eecb6a8
Nick <nsagias@gmail.com>
2023-03-23 15:16:20 -0400

    move to fundamentals folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import './App.css';
 import Alert from './components/alert/Alert';
 import AlertTailwind from './components/alert/AlertTailwind';
-import Animations from './components/animations/Animations';
-import BackgroundAndShadows from './components/backgroundAndShadows/BackgroundAndShadows';
-import Borders from './components/borders/Borders';
-import BreakPoints from './components/breakpoints/BreakPoints';
-import Colors from './components/colors/Colors';
-import Columns from './components/columns/Columns';
-import Containers from './components/containers/Containers';
+import Animations from './components/fundamentals/animations/Animations';
+import BackgroundAndShadows from './components/fundamentals/backgroundAndShadows/BackgroundAndShadows';
+import Borders from './components/fundamentals/borders/Borders';
+import BreakPoints from './components/fundamentals/breakpoints/BreakPoints';
+import Colors from './components/fundamentals/colors/Colors';
+import Columns from './components/fundamentals/columns/Columns';
+import Containers from './components/fundamentals/containers/Containers';
 import DarkMode from './components/darkMode/DarkMode';
-import Fitlers from './components/filters/Filters';
-import FlexAndAlignItems from './components/flex/FlexAndAlignItems';
-import FlexColumnGapAndOrder from './components/flex/FlexColumGapAndOrder';
-import FlexShrink from './components/flex/FlexShrink';
-import Grid from './components/grid/Grid';
-import GridSpans from './components/grid/GridSpans';
-import Interactivity from './components/interactivity/Interactivity';
-import LayoutPosition from './components/layoutPosition/LayoutPosition';
-import Sizing from './components/sizing/Sizing';
-import TransitionAndTransform from './components/transitionAndTransform/TransitionAndTransform';
-import Typography from './components/typography/Typography';
+import Fitlers from './components/fundamentals/filters/Filters';
+import FlexAndAlignItems from './components/fundamentals/flex/FlexAndAlignItems';
+import FlexColumnGapAndOrder from './components/fundamentals/flex/FlexColumGapAndOrder';
+import FlexShrink from './components/fundamentals/flex/FlexShrink';
+import Grid from './components/fundamentals/grid/Grid';
+import GridSpans from './components/fundamentals/grid/GridSpans';
+import Interactivity from './components/fundamentals/interactivity/Interactivity';
+import LayoutPosition from './components/fundamentals/layoutPosition/LayoutPosition';
+import Sizing from './components/fundamentals/sizing/Sizing';
+import TransitionAndTransform from './components/fundamentals/transitionAndTransform/TransitionAndTransform';
+import Typography from './components/fundamentals/typography/Typography';
 
 export default function App() {
   return (
