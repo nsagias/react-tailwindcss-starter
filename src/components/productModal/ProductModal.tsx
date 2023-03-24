d6ccfadf240a37911a8120bf7ec37f9e7ec1143e
Nick <nsagias@gmail.com>
2023-03-23 20:16:44 -0400

    add labal container and label

--- src/components/productModal/ProductModal.tsx
+++ src/components/productModal/ProductModal.tsx
@@ -2,18 +2,39 @@ export default function ProductModal() {
   return (
     <>
       {/* Modal Container */}
-      <div className="flex items-center justify-center min-h-screen bg-slate-300">
+      <div className="flex items-center justify-center min-h-screen bg-slate-100">
         
         {/* Card Container: Product Modal */}
-        <div className="flex flex-col bg-white p-6 m-3 space-y-10 rounded-2xl shadow-2xl md:flex-row md:space-x-10 md:p-16 md:m-0">
+        <div className="flex flex-col p-6 m-3 space-y-10 bg-white rounded-2xl shadow-2xl md:flex-row md:space-y-0 md:space-x-10 md:m-0 md:p-16">
           
-          {/* Image: if not in div, streches */}
+          {/* Image Container: if not in div, streches */}
           <div>
             <img 
               src="images/productModalImages/headphone.png" 
-              alt="headphones" 
-              className="mx-auto hover:scale-105 w-60 duraction-200" />
-          </div>  
+              alt="headphones image" 
+              className="mx-auto hover:scale-105 w-60 duraction-200"
+            />
+          </div> 
+
+            {/* Content */}
+            <div className="flex flex-col space-y-6">
+
+              {/* Label and Decription Container */}
+              <div className="flex flex-col mb-4 space-y-3 text-center md:text-left">
+                {/* Label Container Flex */}
+                <div>
+                  {/* Label */}
+                  <div className="inline-block px-3 py-1 text-sm text-white bg-black rounded-full ">
+                    Free Shipping
+                  </div>
+
+                </div>
+
+              </div>
+
+            </div>
+
+        
         </div>
       </div>
     </>
