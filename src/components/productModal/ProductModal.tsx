217176faabad6d5e3909bcb406002ff78c5b3caa
Nick <nsagias@gmail.com>
2023-03-23 20:29:36 -0400

    add price details

--- src/components/productModal/ProductModal.tsx
+++ src/components/productModal/ProductModal.tsx
@@ -21,17 +21,29 @@ export default function ProductModal() {
 
               {/* Label and Decription Container */}
               <div className="flex flex-col mb-4 space-y-3 text-center md:text-left">
+
                   {/* Label Container Flex */}
                   <div>
+
                     {/* Label */}
                     <div className="inline-block px-3 py-1 text-sm text-white bg-black rounded-full ">
                       Free Shipping
                     </div>
+
                   </div>
+
                   {/* Description */}
                   <div className="max-w-sm text-2xl font-medium">
-                    Lorem ipsum dolor sit amet consectetur.
+                    Lorem ipsum dolor sit amet consec tetur.
                   </div>
+                  
+                  {/* Price Container */}
+                  <div className="flex flex-col mb-4 space-y-3 text-center md:text-left">
+                    <p className="line-through">$988</p>
+                    <p className="text-5xl font-bold">$599</p>
+                    <p className="text-sm font-light tex-gray-400">Lorem ipsum dolor sit amet consectetur  elit. Delectus, commodi!</p>
+                  </div>
+
 
               </div>
 
