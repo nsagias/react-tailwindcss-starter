b6018d9fd5eeb1315955b3092f9931c437c978b7
Nick <nsagias@gmail.com>
2023-03-23 21:03:36 -0400

    add buttons group

--- src/components/productModal/ProductModal.tsx
+++ src/components/productModal/ProductModal.tsx
@@ -61,6 +61,22 @@ export default function ProductModal() {
                     <div className="text-sm">50+ pcs in stock</div>
                   </div>
 
+                  {/* Buttom Buttons Container */}
+                  <div className="flex flex-col space-y-4 md:space-y-0 md:space-x-4 md:flex-row">
+
+                    {/* Left Button */}
+                    <button className="flex items-center justify-center py-3 px-5 space-x-3 border-2 border-gray-300 rounded-lg shadow-sm hover:bg-opacity-30 hover:shadow-lg hover:-translate-y-0.5 transition-all duration-150">
+                      <img src="images/productModalImages/weight.png" alt="" className="w-8" />
+                      <span>Add to cart</span>
+                    </button>
+
+                    {/* Right Button */}
+                    <button className="flex items-center justify-center py-3 px-5 space-x-3 border-2 border-gray-300 rounded-lg shadow-sm hover:bg-opacity-30 hover:shadow-lg hover:-translate-y-0.5 transition-all duration-150">
+                      <img src="images/productModalImages/heart.png" alt="" className="w-8" />
+                      <span>Add to wishlist</span>
+                    </button>
+
+                  </div>
 
               </div>
 
